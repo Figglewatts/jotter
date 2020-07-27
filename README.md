4418376553ea3d4a66bab7238c62bed16280d143
Figglewatts <figglewatts@hotmail.co.uk>
2020-07-27 23:13:57 +0100

    Initial commit of project

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # jotter
 Note taking and syncing utility.
+
+## Design
+
+- `jotter init [REMOTE-URL]`
+  - Create a jotter project (jotterproject.yml).
+  - Git init.
+  - If remote url is given then instead just git clone.
+    - If clone didn't have jotterproject.yml then create one to 'adopt'.
+- `jotter set-remote REMOTE-URL`
+  - Set the git origin to the remote URL.
+- `jotter note NOTE-PATH`
+  - Create a note at the given path and open the configured editor.
+  - If note already existed then open in edit mode.
+  - Wait for close then git add and commit.
+- `jotter sync`
+  - Push/pull changes to/from remote.
+
+- `jotterproject.yml`
+  - `editor`: The command to run to open notes in an editor, i.e. `code` or `vim`.
+  - `note_extension`: The extension to create notes with. Defaults to `.md`.
---
+++ setup.py
@@ -0,0 +1,39 @@
+"""setup.py
+
+The setup script of jotter.
+
+Author:
+    Figglewatts <me@figglewatts.co.uk>
+"""
+
+from os import path
+from setuptools import setup, find_packages
+
+here = path.join(path.dirname(__file__))
+
+
+def get_repo_file_content(filename: str) -> str:
+    with open(path.join(here, filename), encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="jotter",
+    version="0.1.0",
+    description="Note taking and syncing utility.",
+    long_description=get_repo_file_content("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Figglewatts/jotter",
+    author="Figglewatts",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha", "Operating System :: OS Independent"
+    ],
+    keywords="note taking syncing",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "click~=7.1.2", "appdirs~=1.4.4", "marshmallow~=3.7.1", "pyyaml~=5.3.1"
+    ],
+    entry_points={"console_scripts": ["jotter=jotter.cli:main"]},
+)
