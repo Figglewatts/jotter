9e8f01eaefc9c5039e38a24bb9a1874ab49e6414
Figglewatts <figglewatts@hotmail.co.uk>
2020-08-03 17:52:03 +0100

    Actually fix the entry point

--- setup.py
+++ setup.py
@@ -17,26 +17,26 @@ def get_repo_file_content(filename: str) -> str:
         return f.read()
 
 
-setup(name="jotter",
-      version="0.1.2",
-      description="Note taking and syncing utility.",
-      long_description=get_repo_file_content("README.md"),
-      long_description_content_type="text/markdown",
-      url="https://github.com/Figglewatts/jotter",
-      author="Figglewatts",
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Development Status :: 3 - Alpha",
-          "Operating System :: OS Independent"
-      ],
-      keywords="note taking syncing",
-      packages=find_packages(),
-      python_requires=">=3.7",
-      install_requires=[
-          "click~=7.1.2", "appdirs~=1.4.4", "marshmallow~=3.7.1",
-          "pyyaml~=5.3.1"
-      ],
-      entry_points="""
+setup(
+    name="jotter",
+    version="0.1.3",
+    description="Note taking and syncing utility.",
+    long_description=get_repo_file_content("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Figglewatts/jotter",
+    author="Figglewatts",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha", "Operating System :: OS Independent"
+    ],
+    keywords="note taking syncing",
+    packages=find_packages("."),
+    entry_points="""
         [console_scripts]
-        jotter=jotter.cli:main
-    """)
+        jotter=jotter.cli:jotter
+    """,
+    python_requires=">=3.7",
+    install_requires=[
+        "click~=7.1.2", "appdirs~=1.4.4", "marshmallow~=3.7.1", "pyyaml~=5.3.1"
+    ],
+)
